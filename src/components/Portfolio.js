6ac3ff06b3a39c40e1890ef6c775e0475a8be64d
Jaimon <jamin.hogan@gmail.com>
2023-06-07 21:36:18 -0400

    added github repo links to portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import '../styles/Portfolio.css';
 
+const styles = {
+    logos: {
+      maxHeight: "40px",
+      maxWidth: "40px"
+    }
+  }
+
 function Portfolio() {
   return (
   <div>
@@ -14,6 +21,10 @@ function Portfolio() {
               <a href="https://home-hub-management.herokuapp.com/"
               target="_blank" rel="noreferrer">
                   <h3>HomeHub - Property Management Solution</h3>
+                  <object><a href="https://github.com/Anabel-Espinoza/HomeHub">
+                    <img src='./images/gitHub-mark-white.png' 
+                    alt='GitHub Repository for HomeHub' style={styles.logos}/>
+                    </a></object>
                   <img src="./images/hh-home-02.png" 
                   alt="full-stack property management website solution for landlords and tenants" />
               </a>
@@ -21,9 +32,13 @@ function Portfolio() {
 
           {/* <!-- This is my featured project which displays larger --> */}
           <section className="project">
-              <a href="https://barosa19.github.io/marvelous-rental/"
+              <a href="https://jaminhlo.github.io/m2-rentals/"
               target="_blank" rel="noreferrer">
-                  <h3>Marvelous Rentals</h3>
+                  <h4>Marvelous Rentals</h4>
+                  <object><a href="https://github.com/JaminHLO/m2-rentals">
+                    <img src='./images/gitHub-mark-white.png' 
+                    alt='GitHub Repository' style={styles.logos}/>
+                    </a></object>
                   <img src="./images/marvel-rent-01.jpg" 
                   alt="website lists of rental properties and furnitures stores around a map showing their locations" />
               </a>
@@ -34,6 +49,10 @@ function Portfolio() {
               <a href="https://jaminhlo.github.io/weather-dash/"
                   target="_blank" rel="noreferrer">
                   <h4>Weather Dashboard</h4>
+                  <object><a href="https://github.com/JaminHLO/weather-dash">
+                    <img src='./images/gitHub-mark-white.png' 
+                    alt='GitHub Repository' style={styles.logos}/>
+                    </a></object>
                   <img src="https://raw.githubusercontent.com/JaminHLO/weather-dash/main/assets/images/wd-ss01.jpg" 
                   alt="website searchable weather and 5 day forecast" />
               </a>
@@ -44,6 +63,10 @@ function Portfolio() {
               <a href="https://jaminhlo.github.io/daily-planner/"
                   target="_blank" rel="noreferrer">
                   <h4>Work Day Scheduler</h4>
+                  <object><a href="https://github.com/JaminHLO/daily-planner">
+                    <img src='./images/gitHub-mark-white.png' 
+                    alt='GitHub Repository' style={styles.logos}/>
+                    </a></object>
                   <img src="https://github.com/JaminHLO/daily-planner/raw/main/assets/images/dp-ss03.jpg" 
                   alt="website with colorized daily planner" />
               </a>
@@ -54,6 +77,10 @@ function Portfolio() {
               <a href="https://jaminhlo.github.io/coding-quiz/"
                   target="_blank" rel="noreferrer">
                   <h4>Coding Quiz</h4>
+                  <object><a href="https://github.com/JaminHLO/coding-quiz">
+                    <img src='./images/gitHub-mark-white.png' 
+                    alt='GitHub Repository' style={styles.logos}/>
+                    </a></object>
                   <img src="https://raw.githubusercontent.com/JaminHLO/coding-quiz/main/assets/images/cq-ss01.jpg" 
                   alt="website with quiz to test coding knowledge" />
               </a>
@@ -64,6 +91,10 @@ function Portfolio() {
               <a href="https://jaminhlo.github.io/password-generator/"
                   target="_blank" rel="noreferrer">
                   <h4>Password Generator</h4>
+                  <object><a href="https://github.com/JaminHLO/password-generator">
+                    <img src='./images/gitHub-mark-white.png' 
+                    alt='GitHub Repository' style={styles.logos}/>
+                    </a></object>
                   <img src="https://github.com/JaminHLO/password-generator/raw/main/assets/images/pw_ss01.jpg" 
                   alt="website to generate passwords based on user parameters" />
               </a>
